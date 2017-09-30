8532903b7862d93ebb6db431eb3236a3108470a1
Jennifer Cai <yiningcai2014@gmail.com>
2017-09-30 22:15:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 var cart = [];
 
-function getCart() {
- return cart;
+function getCart(){
+  return cart;
 }
 
-function setCart(c) {
+function setCart(c){
   cart = c;
   return cart;
 }
 
+
 function addToCart(item) {
- // write your code here
+  var randomPrice = Math.floor((Math.random()*100)+1)
+  var newItem = {name:item, price:randomPrice}
+  Object.assign({}, newItem)
+  const output = {[item]: randomPrice};
+  cart.push(output)
+  console.log(`${item} has been added to your cart.`)
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var output = []
+  console.log(cart);
+  if (cart.length===0)
+    return "Your shopping cart is empty."
+  for(let i = cart.length; i>0; i--){
+    if(cart.length>3)
+      return "";
+    if (cart.length===2)
+      return(`In your cart you have ${cart[0]} at $${cart[0]} and ${cart[1]} at $${cart[1]}`)
+  }
 }
 
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
-}
+addToCart("bananas")
+addToCart("pancakes")
+addToCart("eggs")
+viewCart()
+//console.log(cart.length)
