223650d8136efe24d95a3042c97d5336d620fae6
Jennifer Cai <yiningcai2014@gmail.com>
2017-10-01 02:50:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,20 +23,23 @@ function viewCart() {
   if (cart.length===0){
     console.log("Your shopping cart is empty.")
   }
+  if(cart.length===1)
+    console.log(`In your cart, you have ${Object.keys(cart[0])[0]} at $${cart[0][objKey]}`)
+  if (cart.length === 2){
+      var item1 = Object.keys(cart[0])[0]
+      var item2 = Object.keys(cart[1])[0]
+      console.log(`In your cart, you have ${item1} at $${cart[0][item1]} and ${item2} at $${cart[1][item2]}.`)
+  }
   for(let i = 0 ; i < cart.length; i++){
     //console.log(`${i} key is ${Object.keys(cart[i])[0]} and value is $${Object.values(cart[i])[0]}`)
    var objKey = Object.keys(cart[i])[0];
    var objValue = cart[i][objKey]
-   if(cart.length===1)
-    console.log(`In your cart you have ${Object.keys(cart[0])[0]} at $${cart[0][objKey]}`)
-    if (cart.length === 2)
-      console.log(`In your cart you have ${Object.keys(cart[0])[0]} at $${cart[0][objKey]} and ${Object.keys(cart[1])[0]} at $${cart[1][objKey]}.`)
-    if(cart.lenght>2) {
+  if(cart.length>2) {
     if (i < (cart.length-1))
       output += `${objKey} at $${objValue}, `;
     else if(i === (cart.length-1))
       output += `and ${objKey} at $${objValue}.`;
-    }
+  }
   }
   console.log(output);
 }
