eeed65025770b84e7dc8aa75567ad56f8b324aa3
Jennifer Cai <yiningcai2014@gmail.com>
2017-10-01 01:39:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,10 +23,11 @@ function viewCart() {
   if (cart.length===0)
     return "Your shopping cart is empty."
   for(let i = 0 ; i < cart.length; i++){
-    //console.log(`${i} key is ${Object.keys(cart[i])[i]} and value is ${Object.values(cart[i])[i]}`)
-    if (i=== cart.length)
-      output += `and ${Object.keys(cart[i])[i]} at ${Object.values(cart[i])[i]}.`
-    else
-      output += `and ${Object.keys(cart[i])[i]} at ${Object.values(cart[i])[i]}, `
-    }
+    //console.log(`${i} key is ${Object.keys(cart[i])[0]} and value is $${Object.values(cart[i])[0]}`)
+    if (i < (cart.length-1))
+      output += `${Object.keys(cart[i])[0]} at $${Object.values(cart[i])[0]}, `;
+    else if(i === (cart.length-1))
+      output += `and ${Object.keys(cart[i])[0]} at $${Object.values(cart[i])[0]}.`;
+  }
+  return output
 }
