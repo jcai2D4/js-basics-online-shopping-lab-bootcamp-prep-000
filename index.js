d6343331ac6034b02dbce5ee8ce3c1c21361e95b
Jennifer Cai <yiningcai2014@gmail.com>
2017-10-01 02:55:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ function viewCart() {
     console.log("Your shopping cart is empty.")
   }
   if(cart.length===1)
-    console.log(`In your cart, you have ${Object.keys(cart[0])[0]} at $${cart[0][objKey]}`)
+    var item1 = Object.keys(cart[0])[0]
+    console.log(`In your cart, you have ${item1} at $${cart[0][item1]}.`)
   if (cart.length === 2){
       var item1 = Object.keys(cart[0])[0]
       var item2 = Object.keys(cart[1])[0]
