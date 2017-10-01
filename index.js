4135b1c17cc22ee5f02552e11246cd6104ada0c9
Jennifer Cai <yiningcai2014@gmail.com>
2017-10-01 02:24:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,15 +19,18 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  var output = "In your cart, you have "
-  if (cart.length===0)
+  if (cart.length===0){
     return "Your shopping cart is empty."
+  }
   for(let i = 0 ; i < cart.length; i++){
     //console.log(`${i} key is ${Object.keys(cart[i])[0]} and value is $${Object.values(cart[i])[0]}`)
+   var objKey = Object.keys(cart[i])[0];
+   var objValue = cart[i][objKey]
     if (i < (cart.length-1))
-      output += `${Object.keys(cart[i])[0]} at $${Object.values(cart[i])[0]}, `;
+      output += `${objKey} at $${objValue}, `;
     else if(i === (cart.length-1))
-      output += `and ${Object.keys(cart[i])[0]} at $${Object.values(cart[i])[0]}.`;
+      output += `and ${objKey} at $${objValue}.`;
   }
   return output
 }
+
